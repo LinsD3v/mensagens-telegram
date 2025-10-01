20f6b6e9959803ccf4fb9a31dc6961fc961a3f5e
LinsD3v <linsd3v@gmail.com>
2025-10-01 20:07:10 -0300

    Last Sync: 2025-10-01 20:07 (Mobile)

--- api/main.py
+++ api/main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 from models import Data
 import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -17,12 +15,11 @@ def create_message(data: Data):
     bot_token = os.getenv("BOT-TOKEN")
     chat_id = os.getenv("CHAT-ID")
 
-    response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", json={
-        "chat_id": chat_id,
-        "text": data.message,
-        "parse_mode": "HTML"
-    })
+    response = requests.post(
+        f"https://api.telegram.org/bot{bot_token}/sendMessage",
+        json={"chat_id": chat_id, "text": data.message, "parse_mode": "HTML"}
+    )
+
     return {"status": "success" if response.status_code == 200 else "error"}
 
-    
-    
+handler = Mangum(app)
