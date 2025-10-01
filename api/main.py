0301a3c5b7fabf7f2bd41f7f61948aef1ba18ecf
unknown <linsd3v#gmail.com>
2025-10-01 11:31:54 -0300

    terminado rota para msg do bot

---
+++ api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from models import Data
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/messages/")
+def create_message(data: Data):
+    bot_token = os.getenv("BOT-TOKEN")
+    chat_id = os.getenv("CHAT-ID")
+
+    response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": data.message,
+        "parse_mode": "HTML"
+    })
+    return {"status": "success" if response.status_code == 200 else "error"}
+
+    
+    
