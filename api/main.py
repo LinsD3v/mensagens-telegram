0142cad7a079eb66148b4e902f7fa64769969654
unknown <linsd3v@gmail.com>
2025-10-02 09:07:22 -0300

    a

--- api/main.py
+++ api/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from models import Data
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
-
-# Modelo de dados (antes estava em models.py, mas podemos deixar aqui pra simplificar)
-class Data(BaseModel):
-    message: str
 
 @app.get("/")
 def read_root():
@@ -15,24 +14,15 @@ def read_root():
 
 @app.post("/messages/")
 def create_message(data: Data):
-    bot_token = os.getenv("BOT_TOKEN")
-    chat_id = os.getenv("CHAT_ID")
+    bot_token = os.getenv("BOT-TOKEN")
+    chat_id = os.getenv("CHAT-ID")
 
-    if not bot_token or not chat_id:
-        return {"status": "error", "detail": "BOT_TOKEN ou CHAT_ID não configurados"}
+    response = requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": data.message,
+        "parse_mode": "HTML"
+    })
+    return {"status": "success" if response.status_code == 200 else "error"}
 
-    response = requests.post(
-        f"https://api.telegram.org/bot{bot_token}/sendMessage",
-        json={
-            "chat_id": chat_id,
-            "text": data.message,
-            "parse_mode": "HTML"
-        }
-    )
     
-
-    return {
-        "status": "success" if response.status_code == 200 else "error",
-        "telegram_response": response.json(),
-        "telegram_url": f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    }
+    
